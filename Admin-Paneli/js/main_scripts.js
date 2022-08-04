d60beee840ce4c1c0955b97d9bbaa7ca934916d9
USoft <usoft.projects@gmail.com>
2022-08-04 18:12:25 +0300

    images changed

--- Admin-Paneli/js/main_scripts.js
+++ Admin-Paneli/js/main_scripts.js
@@ -374,3 +374,12 @@ function image_view(d){
         })
 
 }
+function usoft(){
+    Swal.fire({
+        toast: true,
+        title: 'USoft - <b><u>USoft the clear choice</b></u> ',
+        html:"You can reach us at <a href='mailto:usoft-projects@gmail.com'><b><u> this address.</b></u></a> <br> <p>&copy;Copyright 2022. All Rights Reserved.<br> <b>USoft </b></p>",
+        imageUrl: 'img/rocket.png',
+        imageAlt: 'Custom image',
+      });
+}
